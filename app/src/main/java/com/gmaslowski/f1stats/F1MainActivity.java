5d5b4794440b43c0f089e05a4a8cde9128eea207
Greg Maslowski <gregmaslowski@gmail.com>
2014-10-03 09:15:09 +0200

    Adding empty acivity.

---
+++ app/src/main/java/com/gmaslowski/f1stats/F1MainActivity.java
@@ -0,0 +1,13 @@
+package com.gmaslowski.f1stats;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class F1MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_f1_main);
+    }
+}
